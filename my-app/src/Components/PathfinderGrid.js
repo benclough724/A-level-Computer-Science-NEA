923d0098637da87a47706a452fe17e7df4df7537
Ben Clough <ben_clough123@outlook.com>
2021-09-30 11:17:54 +0100

    college

--- my-app/src/Components/PathfinderGrid.js
+++ my-app/src/Components/PathfinderGrid.js
@@ -1,47 +1,49 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import Node from './Node.js';
 const test = ["Ben", "Joe", "CHris"];
+//const grid = [];
 
-class PathfinderGrid extends Component{
-    constructor(){
-      super()
-      this.state = {
-       // grid: [],
-      };
-    }
-    
-    componentDidMount(){
-      const grid = createGrid();
-     // this.setState({grid});
-    }
-    
-  render(){
-    return(
+class PathfinderGrid extends Component {
+  constructor() {
+    super()
+    this.state = {
+      grid: [],
+    };
+  }
+
+  componentDidMount() {
+    const grid = createGrid();
+    this.setState({ grid });
+  }
+
+  render() {
+    return (
       <div>
-         {grid.map((grid) => {
-           return(
-              <h1>{grid}</h1>
-           );
-         })}
-      </div>
+        {grid.map(() => {
+          return (
+            <Node />
+          );
+        })
+        }
+      </div >
     );
   }
 }
 
 const createGrid = () => {
   const grid = [];
-  for(let row = 0; row < 15; row++){
+  for (let row = 0; row < 15; row++) {
     const currentRow = [];
-    for (let col = 0; col < 50; col++){
+    for (let col = 0; col < 50; col++) {
       currentRow.push();
-      }
-      grid.push(currentRow);
-     // console.log(grid);
-
     }
-    return grid;
+    grid.push(currentRow);
+    console.log(grid);
+
   }
-  
+  return grid;
+}
+
+
 export default PathfinderGrid;
 
-  
