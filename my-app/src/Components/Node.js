22c36ca2c39d580c613849536e47665edd25e4d3
CharlieAIO <39866368+CharlieAIO@users.noreply.github.com>
2021-10-07 16:00:49 +0100

    Update Node.js

--- my-app/src/Components/Node.js
+++ my-app/src/Components/Node.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-export default class Node extends Component {
+export default class Node extends Component{
     constructor(props) {
         super(props);
         this.state = {
             isWall: false,
+            currentCol:props.currentCol,
+            currentRow:props.currentRow
         };
     }
 
     render() {
-        return (
-            <div class='w-30 h-30 outline-black md: flex'>
-                <div className="flex"></div>
-            </div>
-        );
+       return(
+        <button className="p-4 inline border-opacity-100 border-red-400 border-4 w-auto h-auto"
+        onClick={() => alert(`ROW: ${this.state.currentRow}\nCOL: ${this.state.currentCol}`)}>
+        </button>
+    );
     }
 }
-
-//export default Node;
